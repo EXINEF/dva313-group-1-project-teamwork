cee418269c50b749a522f3d8e9fcc5a1c0366d6b
EXINEF <alessio.pierdominici@studenti.unicam.it>
2021-11-22 16:02:32 +0100

    probably the models now are good

--- project/volvoTPMS/models.py
+++ project/volvoTPMS/models.py
@@ -14,9 +14,10 @@ class Sensor(models.Model):
 
 class Tire(models.Model):
     id = models.CharField(max_length=50, primary_key=True)
+    sensor = models.ForeignKey(Sensor, on_delete=models.CASCADE, blank=True, null=True)
+    
     remaining_life = models.IntegerField(blank=True, null=True)
     baseline_pressure = models.IntegerField(blank=True, null=True) 
-    sensor = models.ForeignKey(Sensor, on_delete=models.CASCADE, blank=True, null=True)
     fill_material = models.CharField(max_length=30, blank=True, null=True)
     tread_depth = models.IntegerField(blank=True, null=True)
     revolutions = models.IntegerField(blank=True, null=True)
@@ -24,8 +25,20 @@ class Tire(models.Model):
     def __str__(self):
         return 'Tire: '+self.id
 
+class Company(models.Model):
+    name = models.CharField(max_length=50, blank=True, null=True)
+    description = models.TextField(blank=True, null=True)
+    #vehicles = models.ManyToManyField(Vehicle, blank=True)
+    fleet_managers = models.ManyToManyField(User, blank=True)
+
+    def __str__(self):
+        return self.name
+        
 class Vehicle(models.Model):
     id = models.CharField(max_length=50, primary_key=True)
+    tires = models.ManyToManyField(Tire, blank=True)
+    company = models.ForeignKey(Company,on_delete=models.CASCADE,default=None)
+
     model = models.CharField(max_length=50, blank=True, null=True)
     ambient_temperature = models.IntegerField(blank=True, null=True)
     distance_driven = models.IntegerField(blank=True, null=True)
@@ -34,16 +47,12 @@ class Vehicle(models.Model):
     consumed_fuel = models.FloatField(blank=True, null=True)
     weight = models.FloatField(blank=True, null=True)
     machine_hours = models.IntegerField(blank=True, null=True)
-    tires = models.ManyToManyField(Tire, blank=True)
 
     def __str__(self):
-        return 'Vehicle: '+self.id
+        return 'Vehicle model: '+self.model + ' ID: '+self.id
     
-class Company(models.Model):
-    name = models.CharField(max_length=50, blank=True, null=True)
-    description = models.TextField(blank=True, null=True)
-    vehicles = models.ManyToManyField(Vehicle, blank=True)
-    fleet_managers = models.ManyToManyField(User, blank=True)
+
+    
 
 
 
