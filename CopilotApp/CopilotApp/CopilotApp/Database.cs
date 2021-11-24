182c783e3cbc84d89d87d828749e5a695fb6b390
nsm18002 <58140569+nsm18002@users.noreply.github.com>
2021-11-24 12:29:55 +0100

    Database connection uncertain could just be a emulator/localhost problem, will fix after lunch.

---
+++ CopilotApp/CopilotApp/CopilotApp/Database.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MySql.Data.MySqlClient;
+
+namespace CopilotApp
+{
+    public class Database
+    {
+        static string _ip = "127.0.0.1";
+        static string _port = "3306";
+        static string _username = "root";
+        static string _password = "";
+        static string _database = "dva313";
+
+        static string myConnectionString = "datasource=130.243.94.123;port=3306;username=root;password=;database=dva313";
+        static MySqlConnection mysql_connection;
+
+        public Database()
+        {
+
+        }
+
+        public static void SendData(string tireID, string tirePressure, string tireTemperature)
+        {
+            // 'GetSQLDateTime()'
+
+            string query = "INSERT INTO tire(ID, Pressure, Temperature) VALUES(" + tireID + "," + "," + tirePressure + "," + tireTemperature + ")";
+            SendQuery(query);
+        }
+
+        private string GenerateConnectionString(string ip, string port, string username, string password, string database)
+        {
+            string connection_string = "datasource=" + ip + ";" + "port=" + port + ";" + "username=" + username + ";" + "password=" + password + ";" + "database=" + database;
+            return connection_string;
+        }
+
+        private string GetSQLDateTime()
+        {
+            DateTime myDateTime = DateTime.Now;
+            return myDateTime.ToString("yyyy-MM-dd HH:mm:ss.fff");
+        }
+
+        public static void SendQuery(string query)
+        {
+            mysql_connection = new MySqlConnection(myConnectionString);
+            MySqlCommand sql_cmd = new MySqlCommand(query, mysql_connection);
+            sql_cmd.CommandTimeout = 60;
+
+            try
+            {
+                mysql_connection.Open();
+                MySqlDataReader sql_data_reader = sql_cmd.ExecuteReader();
+
+                if (sql_data_reader.HasRows)
+                {
+                    while (sql_data_reader.Read())
+                    {
+                        Console.WriteLine(sql_data_reader.GetString(0), sql_data_reader.GetString(1), sql_data_reader.GetString(2), sql_data_reader.GetString(3));
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Query failed: " + ex.Message);
+            }
+
+        }
+
+    }
+}
---
+++ CopilotApp/CopilotApp/CopilotApp/MainPageViewmodel.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace CopilotApp
+{
+    public partial class MainPageViewmodel : INotifyPropertyChanged
+    {
+        //Copies of the fields in the XAML because apparently grabbing data from code behind is considered bad practice.
+        //So we'll just keep automatically updated copies here.
+        string _tireID = string.Empty;
+        string _tireTemperature = string.Empty;
+        string _tirePressure = string.Empty;
+        string _tot = string.Empty;
+        public string tireID { get => _tireID; set { _tireID = value; OnPropertyChanged(nameof(tot)); } }
+        public string tirePressure { get => _tirePressure; set { _tirePressure = value; OnPropertyChanged(nameof(tot)); } }
+        public string tireTemperature { get => _tireTemperature; set { _tireTemperature = value; OnPropertyChanged(nameof(tot)); } }
+        public string tot { get => tireID + ", " + tirePressure + ", " + tireTemperature ; set { _tot = value; } }
+
+
+        //Event handler
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        //New command
+        public ICommand OnClickSendDataButton;
+
+        public MainPageViewmodel()
+        {
+            //Binds "OnClickSendDataButton" which is called by the button in XAML to the "SendData" function in this class.
+            OnClickSendDataButton = new Command(SendData);
+        }
+
+        void OnPropertyChanged(string name)
+        {
+            //Some property changed ?? Eventmagic
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+
+        //Calls the Database.cs class and uploads the data.
+        void SendData()
+        {
+            Database.SendData(tireID, tirePressure, tireTemperature);
+        }
+
+    }
+}
