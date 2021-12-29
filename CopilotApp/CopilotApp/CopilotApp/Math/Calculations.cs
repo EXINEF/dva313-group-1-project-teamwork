02004c112a8910c2b013b82f83ed6e5113b22a86
nsm18002 <58140569+nsm18002@users.noreply.github.com>
2021-12-29 15:04:26 +0100

    sync

---
+++ CopilotApp/CopilotApp/CopilotApp/Math/Calculations.cs
@@ -0,0 +1,53 @@
+﻿using MySqlConnector;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CopilotApp
+{
+	public class Calculations
+	{
+
+
+		public Calculations()
+		{
+
+		}
+
+		public async Task run()
+		{
+			//This block will calculate and update DB by indicating if the real site TKPH is under/over specced or neutral.
+			//Every 7th day will the DB be updated.
+			DateTime t_ls = DateTime.Now; //this variable is used to store time since we checked tire lifespan 
+			DateTime endDate = DateTime.Now;
+			DateTime startDate = DateTime.Now;
+			TKPHCalculations tkph = new TKPHCalculations();
+			LifespanCalc ls = new LifespanCalc();
+			
+			while (true)
+			{
+				tkph.Track();
+				endDate = DateTime.Now;
+				t_ls = ls.CalcL(t_ls);
+				if ((endDate - startDate).TotalDays == 7.0)
+				{
+				
+					tkph.Calc();
+					startDate = DateTime.Now;
+					
+				}
+				Thread.Sleep(1000);
+			}
+			await Task.CompletedTask;
+		}
+
+	}
+}
+
+
+
+
+
